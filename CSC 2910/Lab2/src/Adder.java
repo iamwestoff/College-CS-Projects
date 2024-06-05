631049495182f5d107ccf46fec98a3adff6097ef
Easton Herbon <ehh2000.14@gmail.com>
2024-06-05 01:11:40 -0500

    Delete CSC 2910 directory

--- CSC 2910/Lab2/src/Adder.java
+++
@@ -1,5 +0,0 @@
-public class Adder {
-    public static int add(int x, int y) {
-        return x + y;
-    }//end add
-}//end class
--- CSC 2910/Lab2/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-            //Adder a = new Adder();
-        int result = Adder.add(1, 2);
-        System.out.println("The result is: " + result);
-    }
-}
